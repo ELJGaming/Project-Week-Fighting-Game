09394a80b52b12d1dbd22f06f5729ae6bb2e8f60
jahshiekwade58@gmail.com <jahshiek@DESKTOP-3EP0RRM.localdomain>
2023-03-02 01:16:57 -0500

    script.js

--- script/pain.js
+++ script/pain.js
@@ -7,23 +7,45 @@ const startspeech = document.getElementById('speech').play()
 //     },5000);}
 
 
-  const images = ['../img/backgrounds/painimg.jpg', '../img/backgrounds/painimg5.jpg', '../img/backgrounds/painimg6.jpg', '../img/backgrounds/painimg1.jpg', '../img/backgrounds/painimg7.jpg'];
-  let currentImageIndex = 0;
+  // const images = ['../img/backgrounds/painimg.jpg', '../img/backgrounds/painimg5.jpg', '../img/backgrounds/painimg6.jpg', '../img/backgrounds/painimg1.jpg', '../img/backgrounds/painimg7.jpg'];
+  // let currentImageIndex = 0;
   
-  function drawImage() {
-    const img = new Image();
-    img.src = images[currentImageIndex];
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0, 1024, 576);
-    };
+  // function drawImage() {
+  //   const img = new Image();
+  //   img.src = images[currentImageIndex];
+  //   img.onload = function() {
+  //     ctx.drawImage(img, 0, 0, 1024, 576);
+  //   };
     
-    currentImageIndex++;
-    if (currentImageIndex >= images.length) {
-      currentImageIndex = 0;
-    }
+  //   currentImageIndex++;
+  //   if (currentImageIndex >= images.length) {
+  //     currentImageIndex = 0;
+  //   }
+  // }
+  
+  // setInterval(drawImage, 7000); // Switch image every 7 seconds
+  const sentences = ['This is the first sentence.', 'This is the second sentence.', 'This is the third sentence.', 'This is the fourth sentence.', 'This is the fifth sentence.'];
+let currentSentenceIndex = 0;
+
+function drawSentence() {
+  const p = document.createElement('p');
+  p.innerText = sentences[currentSentenceIndex];
+  document.body.appendChild(p);
+  
+  currentSentenceIndex++;
+  if (currentSentenceIndex >= sentences.length) {
+    currentSentenceIndex = 0;
   }
   
-  setInterval(drawImage, 7000); // Switch image every 7 seconds
+  setTimeout(function() {
+    p.remove();
+  }, 5000); // Remove the sentence after 5 seconds
+}
+
+drawSentence();
+
+
+
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
